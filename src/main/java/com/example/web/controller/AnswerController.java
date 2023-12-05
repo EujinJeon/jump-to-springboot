2a391945cdb023b185757a61e4ca5733bec6ddc4
Eujin Jeon <evil0308@gmail.com>
2023-12-05 17:41:46 +0900

    2023-12-05

--- src/main/java/com/example/web/controller/AnswerController.java
+++ src/main/java/com/example/web/controller/AnswerController.java
@@ -1,11 +1,14 @@
 package com.example.web.controller;
 
-import ch.qos.logback.core.model.Model;
 import com.example.web.domain.entity.Question;
 import com.example.web.domain.service.AnswerService;
 import com.example.web.domain.service.QuestionService;
+import com.example.web.validation.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +23,14 @@ public class AnswerController {
     private final AnswerService answerService;
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
-                               @RequestParam String content){
+                               @Valid AnswerForm answerForm, BindingResult bindingResult){
         Question question = this.questionService.getQuestion(id);
         //답변을 저장하는 코드
-        this.answerService.create(question, content);
+        if (bindingResult.hasErrors()){
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
--- src/main/java/com/example/web/controller/QuestionController.java
+++ src/main/java/com/example/web/controller/QuestionController.java
@@ -3,12 +3,14 @@ package com.example.web.controller;
 import com.example.web.domain.entity.Question;
 import com.example.web.domain.entity.QuestionRepository;
 import com.example.web.domain.service.QuestionService;
+import com.example.web.validation.AnswerForm;
+import com.example.web.validation.QuestionForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequestMapping("/question")
@@ -25,9 +27,25 @@ public class QuestionController {
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id){
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm){
+        return "question_form";
+    }
+    
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult)
+    {
+        // 질문 저장하는 코드
+        if (bindingResult.hasErrors()){
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
